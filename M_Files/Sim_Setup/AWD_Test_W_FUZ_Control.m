43df759cb15f34d376651fbb3304bd8b33a9daa3
Kschmutz <kenny.schmutz@keysight.com>
2017-05-25 00:32:08 -0700

    Set up Fuzzy Controller for Split-u Test
    .m file parameters altered to set up vehicle to function properly under
    split-u test.
    Need to increase brake magnitude in attempt to achieve faster braking.
    Changed Fuzzy Controller .mdl to control torque differences from left to
    right. (Cannot exceed 1.1x eachother's min magnitude)
    Valid data can be displayed if sim is ran. (Vehicle should stop around
    11.118s total time)

--- M_Files/Sim_Setup/AWD_Test_W_FUZ_Control.m
+++ M_Files/Sim_Setup/AWD_Test_W_FUZ_Control.m
@@ -77,7 +77,7 @@ Split_u_Time_On = 1;          %Set time for split-u to start (Keep off w large t
 
 Throttle_Step_Time = 1.5;       %Step time of throttle signal
 Throttle_Init_Val = 0;          %Initial throttle value
-Throttle_Final_Val = -1;        %Final throttle value (DOESN'T MATTER)
+Throttle_Final_Val = -0.5;        %Final throttle value (DOESN'T MATTER)
 
 Steering_Input_Select = 1;      %Steering Angle Selection (1 = No SA)
 SA_Start_Time = .2;
@@ -108,7 +108,8 @@ GRR_E = .99;            % Gear reduction efficiency (.95-.99 for Spur/Helical)
 
 %%
 % Fuzzy Controller Settings
-Yaw_Ctrl_Gain = 0.2;
+Yaw_Ctrl_Gain = .5;
+Torque_Diff_Gain = 1.2;
 %%
 
 %% Parameters
