2247e1bdb33fac6c910857f734a0038d24695a4a
Kschmutz <kschmutzz@gmail.com>
2018-04-25 23:10:57 -0700

    Huge changes for final retake of data
    See files.

--- M_Files/Sim_Setup/AWD_Test_W_FUZ_Control.m
+++ M_Files/Sim_Setup/AWD_Test_W_FUZ_Control.m
@@ -101,7 +101,7 @@ r0 = 0.0001;                    % Initial yaw rate [rad/s]
 
 SR_Eq_Accel_Brake_Threshold = 0.001;       %Throttle position threshold for Slip Ratio to change definitions from Accel to Brake or Vice-Versa
 
-T_Avail = 150;          % Peak Torque available by motors [Nm]
+T_Avail = 175;          % Peak Torque available by motors [Nm]
 
 GRR = 10;               % Gear reduction ratio (GRR:1) -> Multiplies torque
 GRR_E = .99;            % Gear reduction efficiency (.95-.99 for Spur/Helical)
@@ -110,18 +110,22 @@ GRR_E = .99;            % Gear reduction efficiency (.95-.99 for Spur/Helical)
 %%
 %%
 % Sliding Mode Controller Settings
-Target_SR = 0.1;
-PWM_Low_Lim = 0.05;
+Target_SR = 0.08;   %ABS Test
+PWM_Low_Lim = 0.0;
 PWM_SW_Threshold = 0;
-Yaw_Ctrl_Gain = 0.2;
-Slip_Err_P_Gain = 0.4;
-Slip_Err_D_Gain = 1.2;     %small D gain results in better (harder stopping) control (0.01) but exeeds limit (SR=0.1)
-Lat_Accel_Err_Gain = 1.6;
 
-Slip_Pass_Band = 200;
-Slip_Stop_Band = 250;
+Slip_Err_P_Gain = 154;
+Yaw_Err_P_Gain = 150;
+Yaw_Ctrl_Gain = 30;
+Lat_Accel_Err_Gain = 0.2;  %calcd from paper as 0.15, but may not work right
+Tau_SRC = 1/(2*pi*10);
+Tau_Yaw = 1/(2*pi*20);
+
+
+%Slip_Pass_Band = 200;
+%Slip_Stop_Band = 250;
 %Motor Model
-z=0.00033;
+z=0.001;
 
 %% Parameters
 g = 9.81;               %   Gravity acceleration [m/s^2]
@@ -153,8 +157,8 @@ Muxs_2 = 0.29;       % sliding friction coefficient
 
 
 % Magic formular (Lateral)
-Ky0 = 90000;        % static cornering stiffness [N/rad]
-Fz0 = 3000;         % static vertical load     [N]
+Ky0 = 90000;        % static cornering stiffness [N/rad]    A_KDS (prev 90000)
+Fz0 = 3000;         % static vertical load     [N]          A_KDS (prev 3000)
 Fz = m*g/4;
 %(Condition 1)
 Muyp0 = 0.85;       % static peak friction coefficient
--- M_Files/Sim_Setup/Automation_Coefficient_Optimizer.m
+++ M_Files/Sim_Setup/Automation_Coefficient_Optimizer.m
@@ -10,17 +10,17 @@ Simulation_Count = 0; %Counts the simulation iteration number
 
 gdp = 0;   %Number of actual saved control gain points (initialized to zero)
 nsp = 50;  %Max number of saved control gain points
-stop_time = 6.001;   %simulation run time (HAS TO BE CHANGED HERE AND ALSO IN MODEL FILE)
+stop_time = 5.501;   %simulation run time (HAS TO BE CHANGED HERE AND ALSO IN MODEL FILE)
 min_rms_YE = repmat(100, 1, nsp);
 min_rms_YE_X = repmat(100, 1, nsp);
-Yaw_Ctrl_Gain_Lowest = repmat(100, 1, nsp);
-Yaw_Ctrl_Gain_Lowest_X = repmat(100, 1, nsp);
-Slip_Err_P_Gain_Lowest = repmat(100, 1, nsp);
-Slip_Err_P_Gain_Lowest_X = repmat(100, 1, nsp);
-Slip_Err_D_Gain_Lowest = repmat(100, 1, nsp);
-Slip_Err_D_Gain_Lowest_X = repmat(100, 1, nsp);
 Lat_Accel_Err_Gain_Lowest = repmat(100, 1, nsp);
 Lat_Accel_Err_Gain_Lowest_X = repmat(100, 1, nsp);
+Slip_Err_P_Gain_Lowest = repmat(100, 1, nsp);
+Slip_Err_P_Gain_Lowest_X = repmat(100, 1, nsp);
+Yaw_Err_P_Gain_Lowest = repmat(100, 1, nsp);
+Yaw_Err_P_Gain_Lowest_X = repmat(100, 1, nsp);
+Yaw_Ctrl_Gain_Lowest = repmat(100, 1, nsp);
+Yaw_Ctrl_Gain_X = repmat(100, 1, nsp);
 
 VMC_Vx = repmat(100, int32(stop_time*1000), nsp);
 VMC_Vy = repmat(100, int32(stop_time*1000), nsp);
@@ -36,43 +36,44 @@ addpath('All_Combined');
 addpath('Fuzzy_Controller_Files');
 
 
-sim_pts = 10;
+sim_pts = 60;
 sim_pts_la = 10;
-for cntr3=sim_pts:-1:1
-    for cntr2=sim_pts:-1:1
-        for cntr=sim_pts:-1:1
-            for cntr1=sim_pts:-1:1
+%for cntr3=sim_pts:-1:1
+    %for cntr2=sim_pts:-1:1
+       %for cntr1=sim_pts:-1:1
+            for cntr=1:sim_pts
                 %update workspace
                 %whos
                 
                 
                 %make edits to sim values
-                %Lat_Accel_Err_Gain = (cntr3/(2*sim_pts_la) + 0.5)*1
-                Lat_Accel_Err_Gain = (cntr/sim_pts)*2
-                Yaw_Ctrl_Gain = (cntr1/sim_pts)*2
-                Slip_Err_P_Gain = (cntr2/sim_pts)*2
-                Slip_Err_D_Gain = (cntr3/sim_pts)*2
-                
+                Lat_Accel_Err_Gain = ??
+                Slip_Err_P_Gain = 154;%35 + 15*cntr
+                Yaw_Err_P_Gain = 3^(-10+cntr/3)
+                Yaw_Ctrl_Gain = ??
                 %simulate and collect data
                 Simulation_Count = Simulation_Count + 1
                 %assignin('base', 'Simulation_Count', Simulation_Count)
                 sim('All_Combined\AWD_EV_MODEL_rev2.mdl')%,'CaptureErrors', 'on')
                 
+                %generate yaw error rms info
+                yaw_rms_err(cntr) = rms(VMC(:,15));
+                Yaw_Err_P_Gain_Saved(cntr) = Yaw_Err_P_Gain;
                 %analyze data and make decision
                 min_rms_YE_new = rms(VMC(:,15));  %Check min rms Yaw error
                 YE_Saved(Simulation_Count) = min_rms_YE_new
                 min_rms_YE
                 if  VMC(4000,16) > 15 ...            %make sure velocity is greater than 5m/s by 5s
-                    && max(VMC(1:5900,16)) >  VMC(6000,16)%...     %make sure Vy lower than 1m/s the entire time.
+                    && max(VMC(1:5400,16)) >  VMC(5500,16)%...     %make sure Vy lower than 1m/s the entire time.
                     %		&& max(VMC(:,18)) < 0.08;    % make sure yaw rate does not exceed 0.08rad(4.5deg)/s
                     for num = 1:(nsp-1)
                         if min_rms_YE_new < min_rms_YE(num);
                             for s = num:nsp
                                 min_rms_YE_X(1,s+1) = min_rms_YE(1,s);
-                                Yaw_Ctrl_Gain_Lowest_X(1,s+1) = Yaw_Ctrl_Gain_Lowest(1,s);
-                                Slip_Err_P_Gain_Lowest_X(1,s+1) = Slip_Err_P_Gain_Lowest(1,s);
-                                Slip_Err_D_Gain_Lowest_X(1,s+1) = Slip_Err_D_Gain_Lowest(1,s);
                                 Lat_Accel_Err_Gain_Lowest_X(1,s+1) = Lat_Accel_Err_Gain_Lowest(1,s);
+                                Slip_Err_P_Gain_Lowest_X(1,s+1) = Slip_Err_P_Gain_Lowest(1,s);
+                                Yaw_Err_P_Gain_Lowest_X(1,s+1) = Yaw_Err_P_Gain_Lowest(1,s);
+                                Yaw_Ctrl_Gain_X(1,s+1) = Yaw_Ctrl_Gain_Lowest(1,s);
                                 
                                 VMC_Vx_X(:,s+1) = VMC_Vx(:,s);
                                 VMC_Vy_X(:,s+1) = VMC_Vy(:,s);
@@ -81,10 +82,10 @@ for cntr3=sim_pts:-1:1
                             end
                             %Place value in 1st slot of dummy (X) arrays
                             min_rms_YE_X(1,num) = min_rms_YE_new;
-                            Yaw_Ctrl_Gain_Lowest_X(1,num) = Yaw_Ctrl_Gain;
-                            Slip_Err_P_Gain_Lowest_X(1,num) = Slip_Err_P_Gain;
-                            Slip_Err_D_Gain_Lowest_X(1,num) = Slip_Err_D_Gain;
                             Lat_Accel_Err_Gain_Lowest_X(1,num) = Lat_Accel_Err_Gain;
+                            Slip_Err_P_Gain_Lowest_X(1,num) = Slip_Err_P_Gain;
+                            Yaw_Err_P_Gain_Lowest_X(1,num) = Yaw_Err_P_Gain;
+                            Yaw_Ctrl_Gain_X(1,num) = Yaw_Ctrl_Gain;
                             
                             VMC_Vx_X(:,num) = VMC(:,16);
                             VMC_Vy_X(:,num) = VMC(:,17);
@@ -93,15 +94,15 @@ for cntr3=sim_pts:-1:1
                             
                             %set newly constructed arrays to variable
                             min_rms_YE = min_rms_YE_X;
-                            Yaw_Ctrl_Gain_Lowest = Yaw_Ctrl_Gain_Lowest_X;
-                            Slip_Err_P_Gain_Lowest = Slip_Err_P_Gain_Lowest_X;
-                            Slip_Err_D_Gain_Lowest = Slip_Err_D_Gain_Lowest_X;
                             Lat_Accel_Err_Gain_Lowest = Lat_Accel_Err_Gain_Lowest_X;
+                            Slip_Err_P_Gain_Lowest = Slip_Err_P_Gain_Lowest_X;
+                            Yaw_Err_P_Gain_Lowest = Yaw_Err_P_Gain_Lowest_X;
+                            Yaw_Ctrl_Gain_Lowest = Yaw_Ctrl_Gain_X;
                             
-                            Gains(1,:) = Yaw_Ctrl_Gain_Lowest_X;
+                            Gains(1,:) = Lat_Accel_Err_Gain_Lowest_X;
                             Gains(2,:) = Slip_Err_P_Gain_Lowest_X;
-                            Gains(3,:) = Slip_Err_D_Gain_Lowest_X;
-                            Gains(4,:) = Lat_Accel_Err_Gain_Lowest_X;
+                            Gains(3,:) = Yaw_Err_P_Gain_Lowest_X;
+                            Gains(4,:) = Yaw_Ctrl_Gain_X;
                             
                             VMC_Vx = VMC_Vx_X;
                             VMC_Vy = VMC_Vy_X;
@@ -121,14 +122,23 @@ for cntr3=sim_pts:-1:1
                     
                 end
             end
-        end
-    end
-end
+        %end
+    %end
+%end
 
 %save workspace to file
-
-Filename_mat = sprintf('Sliding_Mode_Control_CSA_w200kcnrstiffns_w0YawD_Test_%s.mat', datestr(now,'mm-dd-yyyy_HH-MM'));
+Filename_mat = sprintf('Sliding_Mode_Control_CSA_w50kcnrstiffns_Test_%s.mat', datestr(now,'mm-dd-yyyy_HH-MM'));
 save(Filename_mat);
+
+
+
+figure
+hold on
+scatter(Yaw_Err_P_Gain_Saved, yaw_rms_err)
+
+%Save figure
+Filename_yrmserr_v_ygain_fig = sprintf('Sliding_Mode_Control_CSA_w50kcnrstiffns_Test_yrmserr_v_ygain_fig_%s.fig', datestr(now,'mm-dd-yyyy_HH-MM'));
+savefig(Filename_yrmserr_v_ygain_fig);
 
 figure % new figure
 hold on
@@ -152,5 +162,5 @@ for m = 1:gdp
     plot(ax4,VMC(:,10),VMC_YE (:,m))
 end
 
-Filename_fig = sprintf('Sliding_Mode_Control_CSA_Test_w200kcnrstiffns_w0YawD_fig_%s.fig', datestr(now,'mm-dd-yyyy_HH-MM'));
+Filename_fig = sprintf('Sliding_Mode_Control_CSA_w50kcnrstiffns_Test_fig_%s.fig', datestr(now,'mm-dd-yyyy_HH-MM'));
 savefig(Filename_fig);
